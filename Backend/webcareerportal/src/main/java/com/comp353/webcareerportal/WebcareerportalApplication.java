961fc00599e28691ea617d310183014745e7b28b
Dean <deancs1997@gmail.com>
2020-07-29 18:27:16 -0400

    set up backend

---
+++ Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/WebcareerportalApplication.java
@@ -0,0 +1,13 @@
+package com.comp353.webcareerportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebcareerportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebcareerportalApplication.class, args);
+	}
+
+}
---
+++ Backend/webcareerportal/src/test/java/com/comp353/webcareerportal/WebcareerportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comp353.webcareerportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebcareerportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
