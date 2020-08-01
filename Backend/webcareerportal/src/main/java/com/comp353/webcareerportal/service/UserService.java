76e61379e4a7bbd8f6eb781d246048caf02b464b
Dean <deancs1997@gmail.com>
2020-08-01 12:22:06 -0400

    Using new method and can authenticate user

--- Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/service/UserService.java
+++ Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/service/UserService.java
@@ -14,20 +14,25 @@ public class UserService {
     private UserDao userRepo;
 
     public boolean addNewAdmin(Admin admin){
-        if (userRepo.adminExistsWithEmail(admin.getEmail())) return false;
+        if (userRepo.userExistsWithEmail(admin.getEmail())) return false;
          userRepo.save(admin);
          return true;
     }
 
     public boolean addNewJobSeeker(JobSeeker jobSeeker){
-        if (userRepo.jobSeekerExistsWithEmail(jobSeeker.getEmail())) return false;
+        if (userRepo.userExistsWithEmail(jobSeeker.getEmail())) return false;
         userRepo.save(jobSeeker);
         return true;
     }
 
     public boolean addNewEmployer(Employer employer){
-        if (userRepo.employerExistsWithEmail(employer.getEmail())) return false;
+        if (userRepo.userExistsWithEmail(employer.getEmail())) return false;
         userRepo.save(employer);
         return true;
+    }
+
+    public boolean authenticateUser(String employerId, String password){
+        if (!userRepo.userExistsWithEmail(employerId)) return false;
+        return userRepo.authenticateUserWithEmail(employerId, password);
     }
 }
