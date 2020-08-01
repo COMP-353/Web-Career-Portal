4e81086464070daab3a0490b790db20a1bc04ddb
ArsenyKokotov <52434320+ArsenyKokotov@users.noreply.github.com>
2020-08-01 00:52:52 -0400

    removing unnecessary code for login page
    The main layout page  is for the standard layout of all pages. Adding code to it will add code to all pages; thus the code for the login page went to the Index page in the pages folder

--- Frontend/webcareer/src/layouts/MainLayout.vue
+++ Frontend/webcareer/src/layouts/MainLayout.vue
@@ -9,60 +9,7 @@
     </q-header>
 
     <q-page-container>
-      <template>
-        <div class="q-pa-md">
-          <div class="q-gutter-y-md" style="max-width: 60%">
-            <q-card>
-              <q-tabs
-                v-model="tab"
-                active-color="primary"
-                indicator-color="primary"
-                align="justify"
-              >
-                <q-tab name="seeker" label="Job Seeker"></q-tab>
-                <q-tab name="employer" label="Employer"></q-tab>
-                <q-tab name="admin" label="Admin"></q-tab>
-              </q-tabs>
-
-              <!-- <q-separator></q-separator> -->
-
-              <q-tab-panels v-model="tab" animated>
-                <q-tab-panel name="seeker">
-                  <div>
-                    <q-input outlined v-model="text" label="Email Address" />
-                    <q-separator></q-separator>
-                    <q-input outlined v-model="text" label="Password" />
-                    <q-separator></q-separator>
-                    <q-btn color="white" text-color="black" label="Log in" />
-                    <q-btn color="white" text-color="black" label="Sign-Up" />
-                  </div>
-                </q-tab-panel>
-
-                <q-tab-panel name="employer">
-                  <div>
-                    <q-input outlined v-model="text" label="Email Address" />
-                    <q-separator></q-separator>
-                    <q-input outlined v-model="text" label="Password" />
-                    <q-separator></q-separator>
-                    <q-btn color="white" text-color="black" label="Log in" />
-                    <q-btn color="white" text-color="black" label="Sign-Up" />
-                  </div>
-                </q-tab-panel>
-
-                <q-tab-panel name="admin">
-                  <div>
-                    <q-input outlined v-model="text" label="Email Address" />
-                    <q-separator></q-separator>
-                    <q-input outlined v-model="text" label="Password" />
-                    <q-separator></q-separator>
-                    <q-btn color="white" text-color="black" label="Log in" />
-                  </div>
-                </q-tab-panel>
-              </q-tab-panels>
-            </q-card>
-          </div>
-        </div>
-      </template>
+      
 
       <router-view />
     </q-page-container>
