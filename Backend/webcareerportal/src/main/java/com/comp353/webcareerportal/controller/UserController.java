03ecd9b77ec988d7cf1eb1786d7d7161aa14e595
Dean <deancs1997@gmail.com>
2020-08-01 12:22:22 -0400

    new endpoint to authenticate user

--- Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/controller/UserController.java
+++ Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/controller/UserController.java
@@ -5,10 +5,7 @@ import com.comp353.webcareerportal.models.Employer;
 import com.comp353.webcareerportal.models.JobSeeker;
 import com.comp353.webcareerportal.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "user/")
@@ -32,4 +29,10 @@ public class UserController {
     public String addNewJobSeeker(@RequestBody JobSeeker jobSeeker) {
         return userService.addNewJobSeeker(jobSeeker)? "JobSeeker added successfully" : "JobSeeker already exists";
     }
+
+    @GetMapping(path = "authenticateUser/{employerId}")
+    public boolean authenticateUser(@PathVariable String employerId, @RequestBody String password){
+        return userService.authenticateUser(employerId, password);
+    }
+
 }
