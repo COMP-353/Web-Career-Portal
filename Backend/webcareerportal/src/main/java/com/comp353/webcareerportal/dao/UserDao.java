ac01f0bc36599579a105f8d4022998960aa17dc5
Dean <deancs1997@gmail.com>
2020-08-01 12:21:46 -0400

    Added methods
    To authenticate user and new method to check if user exists in all tables at once

--- Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/dao/UserDao.java
+++ Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/dao/UserDao.java
@@ -9,14 +9,21 @@ import java.util.List;
 
 public interface UserDao extends JpaRepository<User, Long> {
 
-//    @Query("select distinct email from admin, employer, jobseeker ")
-//    List<User> getAllUsers();
+    @Deprecated
     @Query("select case when COUNT (e.email) > 0 then true else false end from employer e where e.email = :email")
     boolean employerExistsWithEmail(@Value("email") String email);
 
+    @Deprecated
     @Query("select case when COUNT (js.email) > 0 then true else false end from jobseeker js where js.email = :email")
     boolean jobSeekerExistsWithEmail(@Value("email") String email);
 
+    @Deprecated
     @Query("select case when COUNT (a.email) > 0 then true else false end from admin a where a.email = :email")
     boolean adminExistsWithEmail(@Value("email") String email);
+
+    @Query ("select case when count(e.email) > 0 then true else false end from employer e where e.email in (select e.email from employer e where e.email = :email) or e.email in (select js.email from jobseeker js where js.email = :email) or e.email in (select a.email from admin a where a.email = :email)")
+    boolean userExistsWithEmail(@Value("email") String email);
+
+    @Query("select case when e.password = :password then true else false end from employer e where e.email in (select e.email from employer e where e.email = :email) or e.email in (select js.email from jobseeker js where js.email = :email)or e.email in (select a.email from admin a where a.email = :email)")
+    boolean authenticateUserWithEmail(@Value("email") String email, @Value("password") String password);
 }
