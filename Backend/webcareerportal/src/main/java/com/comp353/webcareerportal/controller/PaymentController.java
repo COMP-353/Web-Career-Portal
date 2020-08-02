8d9dfbf7f6b824641f24e8dbcb8f34b453cd32ed
Dean <deancs1997@gmail.com>
2020-08-02 17:14:21 -0400

    implemented service and endpoint to add methods of payment

--- Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/controller/PaymentController.java
+++ Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/controller/PaymentController.java
@@ -1,4 +1,24 @@
 package com.comp353.webcareerportal.controller;
 
+import com.comp353.webcareerportal.models.CheckingAccount;
+import com.comp353.webcareerportal.models.CreditCard;
+import com.comp353.webcareerportal.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "payment/")
 public class PaymentController {
+    @Autowired
+    PaymentService paymentService;
+
+    @PostMapping(path = "newCheckingAccount/{id}")
+    public boolean addCheckingAccount(@PathVariable(name = "id") String id, @RequestBody CheckingAccount checkingAccount){
+        return paymentService.addCheckingAccount(id, checkingAccount);
+    }
+
+    @PostMapping(path = "newCreditCard/{id}")
+    public boolean addCreditCard(@PathVariable(name = "id") String id,@RequestBody CreditCard creditCard){
+        return paymentService.addCreditCard(id, creditCard);
+    }
 }
--- Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/service/PaymentService.java
+++ Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/service/PaymentService.java
@@ -1,4 +1,42 @@
 package com.comp353.webcareerportal.service;
 
+import com.comp353.webcareerportal.dao.PaymentDao;
+import com.comp353.webcareerportal.dao.UserDao;
+import com.comp353.webcareerportal.models.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PaymentService {
+    @Autowired
+    PaymentDao paymentRepo;
+    @Autowired
+    UserDao userRepo;
+
+    public boolean addCreditCard(String userId, CreditCard creditCard) {
+        if (!setAppropriateUser(userId, creditCard)) return false;
+        paymentRepo.save(creditCard);
+        return true;
+    }
+
+    private boolean setAppropriateUser(String id, Payment payment) {
+        boolean success = false;
+
+        if (userRepo.employerExistsWithEmail(id)) {
+            Employer e = userRepo.getEmployerWithEmail(id);
+            if (e != null) payment.setEmployer(e);
+            success = true;
+        } else if (userRepo.jobSeekerExistsWithEmail(id)) {
+            JobSeeker js = userRepo.getJobSeekerWithEmail(id);
+            payment.setJobSeeker(js);
+            success = true;
+        }
+        return success;
+    }
+
+    public boolean addCheckingAccount(String id, CheckingAccount checkingAccount) {
+        if (!setAppropriateUser(id, checkingAccount)) return false;
+        paymentRepo.save(checkingAccount);
+        return true;
+    }
 }
