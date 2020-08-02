930c5b3194fd6c029f31e79c264fcbf66b8964e6
Dean <deancs1997@gmail.com>
2020-08-02 17:33:52 -0400

    fixed bug
    avoid recursion that caused stack overflow error

--- Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/models/CheckingAccount.java
+++ Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/models/CheckingAccount.java
@@ -14,18 +14,18 @@ public class CheckingAccount extends Payment {
     private String bankNumber;
     private String accountNumber;
 
-    public CheckingAccount(String bankNumber, String accountNumber, boolean automaticWithdrawal, boolean defaultPayment){
+    public CheckingAccount(String bankNumber, String accountNumber, boolean automaticWithdrawal, boolean defaultPayment) {
         this.bankNumber = bankNumber;
         this.accountNumber = accountNumber;
         this.setAutomaticWithdrawal(automaticWithdrawal);
         this.setDefaultPayment(defaultPayment);
     }
 
-    public void setEmployer(Employer e){
-        this.setEmployer(e);
+    public void setEmployer(Employer e) {
+        super.setEmployer(e);
     }
 
-    public void setJobSeeker(JobSeeker js){
-        this.setJobSeeker(js);
+    public void setJobSeeker(JobSeeker js) {
+        super.setJobSeeker(js);
     }
 }
