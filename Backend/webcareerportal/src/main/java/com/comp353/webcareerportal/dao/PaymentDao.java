a4ae38ea795e110529c080e7af2be9036b8decdc
Dean <deancs1997@gmail.com>
2020-08-02 17:14:01 -0400

    added class for repo

--- Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/dao/PaymentDao.java
+++ Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/dao/PaymentDao.java
@@ -1,4 +1,8 @@
 package com.comp353.webcareerportal.dao;
 
-public interface PaymentDao {
+import com.comp353.webcareerportal.models.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentDao extends JpaRepository<Payment, Long> {
+
 }
