ab50a5d58a8becaecb76dbe89be809eb906a8160
Dean <deancs1997@gmail.com>
2020-08-02 14:24:45 -0400

    implemented Payment model

--- Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/models/Payment.java
+++ Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/models/Payment.java
@@ -1,4 +1,27 @@
 package com.comp353.webcareerportal.models;
 
-public class Payment {
+import lombok.Getter;
+import lombok.Setter;
+import sun.util.resources.Bundles;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class Payment {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.AUTO)
+    private int id;
+    private boolean automaticWithdrawal;
+    private boolean defaultPayment;
+
+    @ManyToOne
+    @JoinColumn(name="User")
+    private Employer employer;
+
+    @ManyToOne
+    @JoinColumn(name="User")
+    private JobSeeker jobSeeker;
 }
