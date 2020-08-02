32174a85a6e411c5e236193f61834b790d51577a
Dean <deancs1997@gmail.com>
2020-08-02 17:13:39 -0400

    implemented checking account logic

--- Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/models/CheckingAccount.java
+++ Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/models/CheckingAccount.java
@@ -1,4 +1,31 @@
 package com.comp353.webcareerportal.models;
 
-public class CheckingAccount {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class CheckingAccount extends Payment {
+    private String bankNumber;
+    private String accountNumber;
+
+    public CheckingAccount(String bankNumber, String accountNumber, boolean automaticWithdrawal, boolean defaultPayment){
+        this.bankNumber = bankNumber;
+        this.accountNumber = accountNumber;
+        this.setAutomaticWithdrawal(automaticWithdrawal);
+        this.setDefaultPayment(defaultPayment);
+    }
+
+    public void setEmployer(Employer e){
+        this.setEmployer(e);
+    }
+
+    public void setJobSeeker(JobSeeker js){
+        this.setJobSeeker(js);
+    }
 }
