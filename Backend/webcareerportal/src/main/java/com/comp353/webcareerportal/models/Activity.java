d4359385ccdbaf58f019534dcbc541931dc22bd3
Dean <deancs1997@gmail.com>
2020-08-04 18:45:38 -0400

    Added date stamp for activity

--- Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/models/Activity.java
+++ Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/models/Activity.java
@@ -1,5 +1,6 @@
 package com.comp353.webcareerportal.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,10 +9,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Date;
 
 @Entity
 @Setter
 @Getter
+@AllArgsConstructor
 @NoArgsConstructor
 public class Activity {
 
@@ -20,9 +23,11 @@ public class Activity {
     private int id;
     private String userId;
     private String description;
+    private Date on;
 
     public Activity(String userId, String description){
         this.userId = userId;
         this.description = description;
+        this.on = new Date();
     }
 }
