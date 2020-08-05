00213dde400a4bb63d425e91395edb44a9a12e86
Dean <deancs1997@gmail.com>
2020-08-05 12:45:45 -0400

    to ignore some stupid rules

--- Frontend/webcareer/.eslintrc.js
+++ Frontend/webcareer/.eslintrc.js
@@ -79,7 +79,10 @@ module.exports = {
     quotes: ['warn', 'single', { avoidEscape: true }],
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    
     // allow debugger during development only
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
