1eafa1d85ed61719c8b0439464379e4cc1e8cf21
mano0U7 <sohal001@hotmail.com>
2020-08-05 01:41:37 -0400

    added funcionality to add new application status

--- Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/dao/ApplicationStatusDao.java
+++ Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/dao/ApplicationStatusDao.java
@@ -14,6 +14,9 @@ import com.comp353.webcareerportal.models.JobStatus;
 
 public interface ApplicationStatusDao extends JpaRepository<ApplicationStatus, Long> {
 	
+	@Query("select case when COUNT (a.status) > 0 then true else false end from ApplicationStatus a where a.status = :status")
+    boolean applicationStatusExistsWithStatus(@Value("status") String status);
+	
 	@Query(nativeQuery = true, value = "select * from applicationStatus a where a.statusId= :status_id")
 	ApplicationStatus getApplicationStatusWithId(@Value("statusId") int status_id);
 
---
+++ Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/service/ApplicationStatusService.java
@@ -0,0 +1,23 @@
+package com.comp353.webcareerportal.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.comp353.webcareerportal.dao.ApplicationStatusDao;
+import com.comp353.webcareerportal.dao.JobStatusDao;
+import com.comp353.webcareerportal.models.ApplicationStatus;
+import com.comp353.webcareerportal.models.JobStatus;
+
+@Service
+public class ApplicationStatusService {
+	
+	@Autowired
+    private ApplicationStatusDao applicationStatusRepo;
+	
+	public boolean addNewApplicationStatus(ApplicationStatus applicationStatus) {
+		if (applicationStatusRepo.applicationStatusExistsWithStatus(applicationStatus.getStatus())) return false;
+		applicationStatusRepo.save(applicationStatus);
+		return true;
+	}
+
+}
