7e64477f435f7e95e5f07c9b7b8a7dab461b8107
Dean <deancs1997@gmail.com>
2020-08-05 18:38:47 -0400

    to not truncate our path variables

--- Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/configuration/WebConfig.java
+++ Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/configuration/WebConfig.java
@@ -3,6 +3,7 @@ package com.comp353.webcareerportal.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,4 +14,12 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
     }
+
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.setUseSuffixPatternMatch(false);
+//        configurer.setUseTrailingSlashMatch(false);
+    }
+
 }
