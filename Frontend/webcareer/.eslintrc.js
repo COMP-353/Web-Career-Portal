4409689ba4174a04a45da3f1b846d0f994aedad6
Dean <deancs1997@gmail.com>
2020-08-05 15:42:45 -0400

    vuex plus removing another nasty rule

--- Frontend/webcareer/.eslintrc.js
+++ Frontend/webcareer/.eslintrc.js
@@ -83,6 +83,7 @@ module.exports = {
     '@typescript-eslint/no-unsafe-member-access': 'off',
     '@typescript-eslint/no-unsafe-call': 'off',
     '@typescript-eslint/restrict-plus-operands': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
     
     // allow debugger during development only
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
