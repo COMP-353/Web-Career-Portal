88efe3313d1dd42b0c3be6f7d42df458cecb2ff5
Dean <deancs1997@gmail.com>
2020-08-04 21:18:01 -0400

    Dependency and implementation of email sending

---
+++ Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/service/WcpEmailService.java
@@ -0,0 +1,49 @@
+package com.comp353.webcareerportal.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Properties;
+
+@Component
+public class WcpEmailService {
+
+    public void sendmail(String to, String sub, String t) throws AddressException, MessagingException, IOException {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("webcareerportal@gmail.com", "nysruj-8degmu-gaQsyv");
+            }
+        });
+        Message msg = new MimeMessage(session);
+        msg.setFrom(new InternetAddress("noreply@webcareerportal.com", false));
+
+        msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+        msg.setSubject(sub);
+        msg.setContent("Tutorials point email", "text/html");
+        msg.setSentDate(new Date());
+
+        MimeBodyPart messageBodyPart = new MimeBodyPart();
+        messageBodyPart.setContent(t, "text/html");
+
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(messageBodyPart);
+//        MimeBodyPart attachPart = new MimeBodyPart();
+
+//        attachPart.attachFile("/var/tmp/image19.png");
+//        multipart.addBodyPart(attachPart);
+        msg.setContent(multipart);
+        Transport.send(msg);
+    }
+}
