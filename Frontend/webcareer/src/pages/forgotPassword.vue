42b3dae2a87e8b2b6a510337e2163ab663a7ecf7
Dean <deancs1997@gmail.com>
2020-08-05 12:47:17 -0400

    refactoring

--- Frontend/webcareer/src/pages/forgotPassword.vue
+++ Frontend/webcareer/src/pages/forgotPassword.vue
@@ -6,36 +6,38 @@
 
 <template>
   <q-page padding>
-    
+    <div class="row justify-center full-height full-width text-center">
+      <div class="q-gutter-md" style="max-width: 300px">
+        <div class="q-pa-md q-gutter-sm"></div>
 
-<div class="row justify-center full-height full-width text-center">
-  
-  <div class="q-gutter-md" style="max-width: 300px">
-     <div class="q-pa-md q-gutter-sm">
-  </div>
+        <p>
+          <b
+            >Please input your email. If it matches the one we have in our
+            system, you will see your password in your email.</b
+          >
+        </p>
+        <hr />
+        <q-input filled v-model="email" label="Email" />
+      </div>
+    </div>
 
-    <p><b>Please input your email. If it matches the one we have in our system, you will see your password in your email.</b></p>
-    <hr>
-     <q-input filled v-model="email" label="Email" />
+    <br />
 
-</div>
+    <div class="row justify-center full-height full-width text-center">
+      <div class="clearfix">
+        <q-btn
+          color="white"
+          text-color="black"
+          label="Submit"
+          @click="sendEmail()"
+        />
+        <q-btn to="login" color="white" text-color="black" label="Cancel" />
+      </div>
+    </div>
 
-</div>
-
-<br></br>
-
-<div class="row justify-center full-height full-width text-center">
-<div class="clearfix">
-      <q-btn color="white" text-color="black" label="Submit" @click="sendEmail()"/>
-      <q-btn to="login" color="white" text-color="black" label="Cancel" />
-</div>
-
-</div>
-
-
-<div class="justify-center text-center">
-{{text}}
-</div>
+    <div class="justify-center text-center">
+      {{ text }}
+    </div>
   </q-page>
 </template>
 
@@ -52,7 +54,7 @@ export default {
     sendEmail() {
       axios
         .get('http://localhost:7070/user/forgot/' + this.email)
-        .then(this.done)
+        .then(this.done())
         .catch(e => console.log(e));
     },
     done() {
