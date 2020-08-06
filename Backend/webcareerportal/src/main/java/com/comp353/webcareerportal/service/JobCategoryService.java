d672d61c71ab48edba6cd084e96edfb4e02d046e
Dean <deancs1997@gmail.com>
2020-08-06 09:58:08 -0400

    reformatting

--- Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/service/JobCategoryService.java
+++ Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/service/JobCategoryService.java
@@ -1,33 +1,31 @@
 package com.comp353.webcareerportal.service;
 
-import java.util.List;
-
+import com.comp353.webcareerportal.dao.JobCategoryDao;
+import com.comp353.webcareerportal.models.JobCategory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.comp353.webcareerportal.dao.JobCategoryDao;
-import com.comp353.webcareerportal.models.JobCategory;
-import com.comp353.webcareerportal.models.JobStatus;
+import java.util.List;
 
 @Service
 public class JobCategoryService {
-	
-	@Autowired
+
+    @Autowired
     private JobCategoryDao jobCategoryRepo;
-	
-	public boolean addNewJobCategory(JobCategory jobCategory) {
-		if (jobCategoryRepo.jobCategoryExistsWithCategory(jobCategory.getCategory())) return false;
-		jobCategoryRepo.save(jobCategory);
-		return true;
-	}
-	
-	public boolean deleteJobCategoryWithCategoryId(int id) {
-		if(!jobCategoryRepo.jobCategoryExistsWithCategoryId(id)) return false;
-		jobCategoryRepo.deleteJobCategoryWithCategoryId(id);
-		return true;
-	}
-	
-	public List<JobCategory> getAllJobCategories(){
-		return jobCategoryRepo.getAllJobCategories();
-	}
+
+    public boolean addNewJobCategory(JobCategory jobCategory) {
+        if (jobCategoryRepo.jobCategoryExistsWithCategory(jobCategory.getCategory())) return false;
+        jobCategoryRepo.save(jobCategory);
+        return true;
+    }
+
+    public boolean deleteJobCategoryWithCategoryId(int id) {
+        if (!jobCategoryRepo.jobCategoryExistsWithCategoryId(id)) return false;
+        jobCategoryRepo.deleteJobCategoryWithCategoryId(id);
+        return true;
+    }
+
+    public List<JobCategory> getAllJobCategories() {
+        return jobCategoryRepo.getAllJobCategories();
+    }
 }
