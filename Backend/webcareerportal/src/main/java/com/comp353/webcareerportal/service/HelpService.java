7798a4254c60d84f2754a5fa0ae4092fcc92e8de
Dean <deancs1997@gmail.com>
2020-08-06 10:18:22 -0400

    Added records of helps

--- Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/service/HelpService.java
+++ Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/service/HelpService.java
@@ -1,26 +1,37 @@
 package com.comp353.webcareerportal.service;
 
+import com.comp353.webcareerportal.dao.ActivityDao;
 import com.comp353.webcareerportal.dao.HelpDao;
+import com.comp353.webcareerportal.models.Activity;
 import com.comp353.webcareerportal.models.Help;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class HelpService {
     @Autowired
     private HelpDao helpRepo;
+    @Autowired
+    private ActivityDao activityRepo;
 
-    public void saveHelp(Help help){
-    helpRepo.save(help);
+    public void saveHelp(Help help) {
+        helpRepo.save(help);
+        activityRepo.save(new Activity(help.getJobSeekerId(), "ASKED FOR HELP"));
     }
 
-    public void closeHelp(int id){
-        helpRepo.closeHelpWithId(id);
+    public void closeHelp(int id) {
+        Long idL = new Long(id);
+        Optional<Help> help = helpRepo.findById(idL);
+        if (help.isPresent()) {
+            helpRepo.closeHelpWithId(id);
+            activityRepo.save(new Activity(help.get().getEmployerId(), "CLOSED A HELP TICKET"));
+        }
     }
 
-    public List<Help> getAllOpenHelpForEmployerWithId(String id){
+    public List<Help> getAllOpenHelpForEmployerWithId(String id) {
         return helpRepo.getAllOpenHelpForEmployerWithId(id);
     }
 }
