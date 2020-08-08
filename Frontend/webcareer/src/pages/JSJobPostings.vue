8ccaef58f8adcbdcd779be70987c090820e54869
ArsenyKokotov <52434320+ArsenyKokotov@users.noreply.github.com>
2020-08-08 17:23:59 -0400

    Update JSJobPostings.vue
    added pop for help; not yet connected to backend

--- Frontend/webcareer/src/pages/JSJobPostings.vue
+++ Frontend/webcareer/src/pages/JSJobPostings.vue
@@ -1,28 +1,26 @@
 <style lang="sass" scoped>
 .my-card
+    width: 100%
+    max-width: 250px
 </style>
 
 <template>
   <q-layout view="hHh LpR fFf">
     <JSHeader />
-    <router-view />
+      <router-view />
 
-    <q-page-container
-      style="height: 100%; padding-right: 1%; padding-left: 1%;"
-    >
-      <div style="padding-top: 1%; padding-bottom: 1%;">
-        <q-card flat bordered class="my-card">
-          <q-card-section>
-            <div class="text-h6">Welcome back {{ getGreetingField() }}!</div>
-          </q-card-section>
-          <q-card-section
-            ><p>These are the list of jobs you can apply to.</p></q-card-section
-          >
-        </q-card>
-      </div>
+    <q-page-container style="height: 100%;">
+      <q-card flat bordered class="my-card">
+        <q-card-section>
+          <div class="text-h6 ">
+            Welcome back {{ getGreetingField()}}!
+          </div>
+        </q-card-section>
+      </q-card>
+
       <div class="row">
         <div class="col">
-          <div class="q-pa-md">
+          <div class="q-pl-xl">
             <q-markup-table>
               <thead>
                 <tr>
@@ -33,16 +31,17 @@
                   <th class="text-left">Category</th>
                   <th class="text-left">Status</th>
                   <th class="text-right">Apply!</th>
+		  <th class="text-right">Help!</th>
                 </tr>
               </thead>
               <tbody>
-                <tr v-for="job in jobList" v-bind:key="job.jobId">
-                  <td class="text-left width:10">{{ job.jobId }}</td>
-                  <td class="text-left">{{ job.title }}</td>
-                  <td class="text-left">{{ job.employer.email }}</td>
-                  <td class="text-left">{{ job.description }}</td>
-                  <td class="text-left">{{ job.jobCategory.category }}</td>
-                  <td class="text-left">{{ job.jobStatus.status }}</td>
+                <tr v-for='job in jobList' v-bind:key='job.jobId'>
+                  <td class="text-left width:10">{{job.jobId}}</td>
+                  <td class="text-left">{{job.title}}</td>
+                  <td class="text-left">{{job.employer.email}}</td>
+                  <td class="text-left">{{job.description}}</td>
+                  <td class="text-left">{{job.jobCategory.category}}</td>
+                  <td class="text-left">{{job.jobStatus.status}}</td>
                   <td class="text-right">
                     <div class="q-pa-md q-gutter-sm">
                       <q-btn
@@ -54,22 +53,61 @@
                       />
                     </div>
                   </td>
+		    <td class="text-right">
+                    <div class="q-pa-md q-gutter-sm">
+                      <q-btn
+			@click="someHelp=true"
+                        label="Help "
+                        size="sm"
+			color="orange"
+                        icon="send"
+                      />
+                    </div>
+                  </td>
                 </tr>
               </tbody>
             </q-markup-table>
+
+<q-dialog v-model="someHelp">
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">Help</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <p>What is your issue?</p>
+	  <q-input
+                v-model="Help.info"
+                label="Description"
+                outlined
+                type="textarea"
+                :rules="[
+                (val) => (val && val.length > 0 || this.accept ==false) || 'Please provide a description of the job',
+              ]"
+              />
+        </q-card-section>
+
+ <q-card-actions align="right">
+          <q-btn flat @click="CreateHelp()" label="Submit" color="primary" v-close-popup />
+	  <q-btn flat label="Cancel" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
+
             <q-dialog v-model="showDialog">
-              <q-card>
-                <q-card-section>
-                  <div class="text-h6">Application Sent</div>
-                </q-card-section>
+                  <q-card>
+                      <q-card-section>
+                        <div class="text-h6">Application Sent</div>
+                      </q-card-section>
 
-                <q-card-section class="q-pt-none">
-                  Your application has been sent to the employer.
-                </q-card-section>
+                    <q-card-section class="q-pt-none">
+                      Your application has been sent to the employer.
+                    </q-card-section>
 
-                <q-card-actions align="right">
-                  <q-btn flat label="OK" color="green" v-close-popup></q-btn>
-                </q-card-actions>
+                  <q-card-actions align="right">
+                    <q-btn flat label="OK" color="green" v-close-popup></q-btn>
+                  </q-card-actions>
               </q-card>
             </q-dialog>
           </div>
@@ -92,6 +130,10 @@ export default {
     },
   data() {
     return {
+      someHelp: false,
+      Help: {
+      info:''
+      },
       showDialog:false,
       creditcard: 'automatic',
       checkingacc: 'automatic',
@@ -141,6 +183,14 @@ export default {
   },
 
   methods: {
+
+    CreateHelp(){
+    axios
+        .post(
+          this.baseUrl+'help/'
+        )
+},
+
     assignJsObject(res) {
       console.log(res)
       this.jobSeeker.firstName = res.firstName;
@@ -179,6 +229,9 @@ export default {
     getGreetingField(){
       return this.jobSeeker.firstName != null ? this.jobSeeker.firstName : this.jobSeeker.email;
     },
+
+
+    
 
     createApplication(jobId){
       this.application.job.jobId = jobId;
