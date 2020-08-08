bbda2fd832f201d09790c9675f28e5a4569ec55e
Dean <deancs1997@gmail.com>
2020-08-08 01:14:01 -0400

    to ssh connection

--- Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/WebcareerportalApplication.java
+++ Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/WebcareerportalApplication.java
@@ -1,15 +1,26 @@
 package com.comp353.webcareerportal;
 
+import com.comp353.webcareerportal.configuration.SSHConnection;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 @EnableScheduling
 public class WebcareerportalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebcareerportalApplication.class, args);
-	}
+    SSHConnection sshConnection;
+
+    public static void main(String[] args) throws SQLException {
+
+        SSHConnection sshConnection = new SSHConnection();
+        SpringApplication.run(WebcareerportalApplication.class, args);
+
+    }
+
 
 }
+
+
--- Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/configuration/MyContextListener.java
+++ Backend/webcareerportal/src/main/java/com/comp353/webcareerportal/configuration/MyContextListener.java
@@ -1,2 +1,38 @@
-package com.comp353.webcareerportal.configuration;public class MyContextListener {
+package com.comp353.webcareerportal.configuration;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class MyContextListener implements ServletContextListener {
+
+    private SSHConnection connectionSSH;
+
+
+    public MyContextListener() {
+        super();
+    }
+
+    /**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent arg0) {
+        System.out.println("Context initialized ... !");
+        try {
+            connectionSSH = new SSHConnection();
+        } catch (Throwable e) {
+            e.printStackTrace(); // error connecting SSH server
+        }
+    }
+
+    /**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent arg0) {
+        System.out.println("Context destroyed ... !");
+        connectionSSH.closeSSHConnection(); // disconnect
+    }
+
 }
+
